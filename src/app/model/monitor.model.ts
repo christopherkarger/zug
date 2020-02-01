a2954f1a163ee6ab2bd7ee5237943de4d50d2be6
Christopher Karger <karger1986@gmail.com>
2020-02-01 08:48:43 +0100

    added model#

---
+++ src/app/model/monitor.model.ts
@@ -0,0 +1,13 @@
+export interface Ijourney {
+  ti: string; // Abfahrtszeit "08:04"
+  da: string; // Datum "01.02.2020"
+  lastStop: string; //Endstation "Wien Hbf"
+  tr: string; // Bahnsteig "2"
+  rt: { dlt: string } | false; // Verzögerung "dlt:11:01"
+}
+
+export interface IMonitor {
+  stationName: string; // Bahnhofsstation "Wr.Neustadt Hbf"
+  boardType: string; // Ankunft oder Abfahrt "dep"
+  journey: Ijourney[]; // Einzelne Zeiten
+}
