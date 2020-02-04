0a7756d0bc53c9ee800ff540f2f1f52158b5670f
Christopher Karger <karger1986@gmail.com>
2020-02-04 21:34:51 +0100

    cleaned station

--- src/app/clean-station-name.pipe.ts
+++ src/app/clean-station-name.pipe.ts
@@ -6,7 +6,14 @@ import { Pipe, PipeTransform } from "@angular/core";
 export class CleanStationNamePipe implements PipeTransform {
   transform(value: any, ...args: any[]): any {
     let convertedStr = value;
-    convertedStr = convertedStr.replace("Wien", "").replace("Bahnhst", "");
+    convertedStr = convertedStr
+      .replace("Bahnhst", "")
+      .replace("&#38;", "&")
+      .replace("&#223;", "ß")
+      .replace("&#196;", "Ä")
+      .replace("&#228;", "ä")
+      .replace("&#214;", "Ö")
+      .replace("&#246;", "ö");
     return convertedStr;
   }
 }
