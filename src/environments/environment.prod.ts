785ed22453a16a063fc27760715e92cfed6e653e
Christopher Karger <karger1986@gmail.com>
2020-02-04 19:53:51 +0100

    build script

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   api: {
     domain: "",
-    leave: "/oebb.php?evaId=${0}&dirInput=${1}"
+    leave: "/zug/oebb.php?evaId=${0}&dirInput=${1}"
   }
 };
